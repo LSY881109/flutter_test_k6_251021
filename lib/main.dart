38c21900383a13793ae4fbfc34e7b33f49012653
이상용 <lsy3709@naver.com>
2025-10-24 11:40:31 +0900

    3교시, 커스텀 화면 작업4. 기본 라우팅2, 스플래쉬화면 3초 보여주고 메인 이동하기. 스테이트 풀 위젯의 생명주기 함수를 이용해서 사용함.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:busanit501_flutter_workspace_251021/screen/login_screen.dart';
 import 'package:busanit501_flutter_workspace_251021/screen/main_screen.dart';
 import 'package:busanit501_flutter_workspace_251021/screen/my_app_routing.dart';
 import 'package:busanit501_flutter_workspace_251021/screen/my_splash.dart';
+import 'package:busanit501_flutter_workspace_251021/screen/my_splash2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   // runApp( MySplash() ); // runApp
   // runApp( MyLoginScreen() ); // runApp
   // runApp( MyMainScreen() ); // runApp
-  runApp( MyAppRouting() ); // runApp
+  // runApp( MyAppRouting() ); // runApp
+  runApp( MySplash2() ); // runApp
 } // main()
