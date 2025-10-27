3a3f402f3208da442eaca5d2cf2d9c462ab58d2b
이상용 <lsy3709@naver.com>
2025-10-27 10:17:44 +0900

    2교시, 공공 데이터 연동 작업 1, 컨트롤러, 모델, 화면 나누고, 모델, 컨트롤러 작업 중

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,13 @@ import 'package:busanit501_flutter_workspace_251021/screen/my_splash.dart';
 import 'package:busanit501_flutter_workspace_251021/screen/my_splash2.dart';
 import 'package:flutter/material.dart';
 
+import 'screen/sample_my_app1.dart';
+
 void main() {
   // runApp( MySplash() ); // runApp
   // runApp( MyLoginScreen() ); // runApp
   // runApp( MyMainScreen() ); // runApp
   runApp( MyAppRouting() ); // runApp
+  // runApp( MyApp() ); // runApp
   // runApp( MySplash2() ); // runApp
 } // main()
